f2d2afb521b99116eb94f8396250c9d8dab175db
BergListe <guinness@gmx.at>
2012-01-16 13:21:57 +0100

    Changed path to switch.config

--- js/corpusshell.config.php
+++ js/corpusshell.config.php
@@ -1,5 +1,5 @@
 <?php
-  $configUrl = "../../sru/switch.config";
+  $configUrl = "../sru/switch.config";
   $localhost = "corpus3.aac.ac.at";
 
   function GetNodeValue($node, $tagName)
@@ -12,23 +12,23 @@
      }
      return "";
   }
-  
+
   header("Content-Type: application/x-javascript");
   print "var SearchConfig = new Array();\n";
-  
+
   $doc = new DOMDocument;
   $doc->Load($configUrl);
 
   $xpath = new DOMXPath($doc);
   $query = '//item';
   $entries = $xpath->query($query);
-  
+
   $idx = 0;
 
   foreach ($entries as $entry)
   {
      print "SearchConfig[$idx] = new Array();\n";
-     
+
      $name = GetNodeValue($entry, "name");
      print "SearchConfig[$idx]['x-context'] = '$name';\n";
      $label = GetNodeValue($entry, "label");
