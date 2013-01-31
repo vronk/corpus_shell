4159acddc4936d6b1637cee41d6563fa6e74a136
vronk <tech@vronk.net>
2013-01-31 17:21:58 +0100

    added link-widget, generate link from settings moved docs inside the jquery closure

--- scripts/js/query_input/qi.js
+++ scripts/js/query_input/qi.js
@@ -1,3 +1,7 @@
+
+// jQuery closure 
+(function($) {
+
 /**
  * @class QueryInput
  * generate a customizable query_input UI, a input form with fields/widgets based on settings
@@ -9,14 +13,8 @@
  * @author vronk, Andy Basch
  * @version 2013-01-17
  */
-
-
 /* we could make it a class (instead of jQuery plugin)
-       function QueryInput(elem,s) */ 
-       
-// jQuery closure 
-(function($) {
-
+       function QueryInput(elem,s) */     
 $.fn.QueryInput = function (options)
 {
    /** the dom-element to generate the query input in */
@@ -52,7 +50,7 @@ $.fn.QueryInput = function (options)
          } else if (trg_container)  {
             var label= param.label ? param.label : key;
             var new_input_label = param.label=='' ? '' : "<label>" + label+ "</label>";
-            var new_input, new_widget=null;
+            var new_input=null, new_widget=null;
             
             switch (param.widget) {
                 case "text":
@@ -67,6 +65,9 @@ $.fn.QueryInput = function (options)
                 case "autocomplete":
                   new_input = genAutocomplete (key, param);    
                   break;
+                case "link":
+                  new_input = genLink (key, param);    
+                  break;
                 case "slider":
                   var slider_elems = genSlider(key, param);
                   new_input = slider_elems[0];
@@ -75,23 +76,30 @@ $.fn.QueryInput = function (options)
                 default:
                   console.log("no such widget: " + param.widget); 
               }    
-                  
-         // set initial value
-         $(new_input).val(param.value)
-                     .data("key", key)
-                     .attr("id", settings.input_prefix + key)
-                     .data("param-object", param);
-             
-        /* update settings and widgets upon value-change */
-        new_input.change(function () {
-                   setParamValue(this);
-             });
-                   
-            $(form).append(new_input_label, new_input, new_widget);
-           // inputs.push(key, new_input_label, new_input, new_widget]);
-            
+        
+          if (new_input) {
+            $(new_input).data("key", key)
+                        .addClass("type-" + param.widget)
+                             .attr("id", settings.input_prefix + key)
+                             .data("param-object", param);
+                     
+            if (param.widget!="link") {
+                 // set initial valuelink
+                 $(new_input).val(param.value);
+                     
+                /* update settings and widgets upon value-change */
+                new_input.change(function () {
+                           setParamValue(this);
+                     });
+               }            
+               
+             $(form).append(new_input_label, new_input, new_widget);
+            // inputs.push(key, new_input_label, new_input, new_widget]);
+           }
          }
-       }
+       } // end for
+       
+       
     }
 /*
     function formatForm(inputs) {
@@ -110,6 +118,18 @@ $.fn.QueryInput = function (options)
         return input;
     }
     
+    function genLink(key, param_settings) {
+         
+        var a = $("<a>" + key + "</a>");
+        a.attr("href", generateLink());
+       
+            $(a).mousedown(function(e) {
+                $(this).attr("target", "_blank");
+                $(this).attr("href", generateLink());
+         });
+        return a;
+    }
+    
     function genSubmit(key, param_settings) {
          
         var input = $("<input type='submit'/>");
@@ -118,7 +138,7 @@ $.fn.QueryInput = function (options)
         return input;
     }
         
-    /** generating out own comboboxes, because very annoying trying to use some of existing jquery plugins (easyui.combo, combobox, jquery-ui.autocomplete) */ 
+    /** generating ou own comboboxes, because very annoying trying to use some of existing jquery plugins (easyui.combo, combobox, jquery-ui.autocomplete) */ 
     function genCombo (key, param_settings) {
     
         var select = $("<select id='widget-" + key + "' />")
@@ -196,7 +216,8 @@ $.fn.QueryInput = function (options)
         accepting settings as reliable source of current value
         i.e. changes in input have to change the .value in settings.
         This is assured by calling setParamValue upon input-change.
-        @public by calling: $(elem).data("qi").getParamValue(param_key);
+        public by calling: $(elem).data("qi").getParamValue(param_key);
+        @public 
     */
     settings.getParamValue = function(key) {
          if (this.params[key])  {
@@ -206,6 +227,17 @@ $.fn.QueryInput = function (options)
          }
     }
 
+    /** allow to update the current value in settings from outside */
+    settings.setParamValue = function(key, value) {
+        
+        if (settings.params[key]) {
+            settings.params[key].value = value
+        }
+        // callback:
+        settings.onValueChanged.call( settings,value );
+        return value    
+    }
+    
     /** update the current value in settings */
     function setParamValue(input_object) {
         var param_object = $(input_object).data("param-object");
@@ -249,6 +281,20 @@ $.fn.QueryInput = function (options)
        for ( var key in settings_params ) {
             if (params[key]) {settings_params[key].value=params[key]; }      
        }
+    }
+
+ /** generate a query-link out of the current values of the params */
+    function generateLink()
+    {
+      var url="?";
+      
+       for ( var key in settings.params) {
+           if (settings.params[key].value) {
+                 url += key + "=" + settings.params[key].value + "&"; 
+             }      
+       }
+       
+      return url;
     }
 
 
