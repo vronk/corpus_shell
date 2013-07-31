6332d95db95752fe6d562c9df49e0b749e622835
Omar Siam <simar@gmx.net>
2013-07-31 21:14:15 +0200

    Add param.js pointing at a CORS enabled URL for switch.

---
+++ scripts/js/params.js
@@ -0,0 +1,9 @@
+
+/**
+ * @fileOverview a default version of the params-file for instance specific configuration.
+ * Makes things work just pointing a CORS compatible Browser (non-IE, IE >= 10) at the
+ * local copy. The switch URL is a temporary reverse proxy solution.
+ */
+
+var baseURL = ".";
+var switchURL = "http://www.siam.homeunix.net/switch";
