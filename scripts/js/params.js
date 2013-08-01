3c932e147d2d5ca8fb3a7bc8dd7f0b734d1e58be
Omar Siam <simar@gmx.net>
2013-08-01 15:02:00 +0200

    Added mussing json file, added missing parameter userData.

--- scripts/js/params.js
+++ scripts/js/params.js
@@ -5,5 +5,19 @@
  * local copy. The switch URL is a temporary reverse proxy solution.
  */
 
+/**
+ * @module corpus_shell 
+ */
+
+/** Base url or where do we live. Does not make much sense on a locally downloaded copy.
+ * @type {path|url}  
+ */
 var baseURL = ".";
+/** The place where the php scripts for user data management live (see the modules/userdata sub directory). It's assumend this is a path which needs to be added to baseUrl.
+ * @type {path}
+ */
+var userData = "/modules/userdata/";
+/** The url of the switch script. May be anywhere on the internet if cross-origin resource shareing (CORS, see enable-cors.org) is properly set up.
+ * @type {url} 
+ */
 var switchURL = "http://www.siam.homeunix.net/switch";
