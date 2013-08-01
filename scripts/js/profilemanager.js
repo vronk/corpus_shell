91bdb3fdc87e8d23a6dd8fd221c0ea6f807d3a73
Omar Siam <simar@gmx.net>
2013-08-01 18:17:52 +0200

    Comments for all the methods in profilemanager.js.

--- scripts/js/profilemanager.js
+++ scripts/js/profilemanager.js
@@ -8,23 +8,45 @@
  */
 
 /**
- * @classdesc A class for managing the profiles that contain queries and their layout.
+ * @classdesc A class for managing the profiles that contain queries, windows and their layout.
  * @constructor 
  */
 function ProfileManager()
 {
+  /**
+   * A "map" (that is an object with ever extending dynamic properties) of maps for
+   * {@link MainPanelObject}s.
+   * @type {map.<map.<MainPanelObject>>} 
+   */
   this.Profiles = new Array();
 
+  /**
+   * @param {string} name Name of the profile.
+   * @param {map.<MainPanelObject>} profile A Profile to set.
+   * @desc Set a particular profile by name
+   * @return -
+   */
   this.SetProfile = function(name, profile)
   {
     this.Profiles[name] = profile;
   }
 
+  /**
+   * @param {string} name New name of the profile.
+   * @param {map.<MainPanelObject>} profile A Profile to clone.
+   * @desc Set a clone of a particular profile by name
+   * @return -
+   */
   this.SetProfileAsNew = function(name, profile)
   {
     this.Profiles[name] = this.CloneProfile(profile);
   }
 
+  /**
+   * @param {string} name Name of the profile.
+   * @desc Retrieve a profile by name.
+   * @return {map.<MainPanelObject>} The profile stored by this name. If there is now profile stored a new empty object is returned.
+   */
   this.GetProfile = function(name)
   {
     var profile = this.Profiles[name];
@@ -34,6 +56,12 @@ function ProfileManager()
     return profile;
   }
 
+  /**
+   * @param {string} name Name of the profile.
+   * @param {string} newName New name of the profile.
+   * @desc Rename a profile.
+   * @return -
+   */
   this.RenameProfile = function(name, newName)
   {
     var profile = this.Profiles[name];
@@ -41,6 +69,11 @@ function ProfileManager()
     delete this.Profiles[name];
   }
 
+  /**
+   * @param {string} name Name of the profile.
+   * @desc Delete the profile stored by this name.
+   * @return -
+   */
   this.DeleteProfile = function(name)
   {
     var profile = this.Profiles[name];
@@ -48,6 +81,11 @@ function ProfileManager()
       delete this.Profiles[name];
   }
 
+  /**
+   * @desc Retrieves the name of all profiles stored in {@link module:corpus_shell~ProfileController}.
+   * That is not necessaryly the list of all profiles in this ProfileManager.
+   * @return array.<string> The profile names 
+   */
   this.GetProfileNames = function()
   {
     var list = new Array();
@@ -59,6 +97,11 @@ function ProfileManager()
     return list;
   }
 
+  /**
+   * @param {map.<MainPanelObject>} profile A profile to clone.
+   * @desc Creates an independent clone, a deep copy, of the given profile.
+   * @return {map.<MainPanelObject>} The cloned profile.
+   */
   this.CloneProfile = function(profile)
   {
     var newProfile = jQuery.extend(true, {}, profile);
@@ -67,6 +110,6 @@ function ProfileManager()
 }
 
 /**
- * The ProfileController object. 
+ * The ProfileController object: the central instance of {@link module:corpus_shell~ProfileManager}
  */
 var ProfileController = new ProfileManager();
