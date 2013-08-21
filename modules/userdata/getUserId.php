f86cec3e481e3b0837b05baae90ba780f568de79
simar <simar@gmx.net>
2013-08-21 17:41:45 +0200

    Added documentation (phpdoc standard) to the central config file (it's dist version respectively) and to the scripts managing server side storage of corpus_shell.

--- modules/userdata/getUserId.php
+++ modules/userdata/getUserId.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Generates a GUID for identifying a particular corpus_shell user's 
+ * persistence file on this web server
+ * 
+ * A utility service which is otherwise unrelated to the storage and retrieval of
+ * the data even though the identifier will be used as a part of the filename
+ * on the server. The data is presented to the client as a JSON object with a
+ * property of "id". The browser is advised to refetch such GUIDs every time.
+ * @see getUserData.php
+ * @see saveUserData.php
+ * @package user-data
+ */
+ 
+ /**
+  * Creates a GUID based on the time and origin of the request.
+  */
  function create_guid($namespace = '')
  {
     $guid = '';
