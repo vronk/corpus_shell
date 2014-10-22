6d376fe647528bb0183834f9cfe5908628d95800
Omar Siam <Omar.Siam@oeaw.ac.at>
2014-10-22 15:46:28 +0200

    Infrastructure for automated XSLT tests and first tests.

--- tests/xslt/xsltTest.php
+++ tests/xslt/xsltTest.php
@@ -14,7 +14,55 @@ require_once __DIR__ . '/../common/XPathTestCase.php';
 require_once __DIR__ . '/../common/switchParts.php';
 
 class XSLTTests extends XPathTestCase {
-
+    /**
+     * @test
+     */
+    public function it_should_transform_a_switch_explain() {
+        // html of switches explain has no wrapping div, so it's not valid xml.
+        // Use the full page version instead.
+        global $sru_fcs_params;
+        global $scriptsUrl;
+        
+        $sru_fcs_params->xformat = 'htmlpagetable';
+        $scriptsUrl = 'http://corpus3.aac.ac.at/vicav2/corpus_shell/scripts/';
+        $this->doAssertTransformEqualsExpectedIndented("switch-explain",
+                '', 'explain');
+    }
+    
+    /**
+     * @test
+     */
+    public function it_should_transform_a_switch_resources_scan() {
+        $this->doAssertTransformEqualsExpectedIndented("switch-resource-scan",
+                '', 'scan', 'fcs.resource');
+    }
+    
+    /**
+     * @test
+     */
+    public function it_should_transform_a_vicav_profile_explain() {
+        $this->doAssertTransformEqualsExpectedIndented("vicav_profile_explain",
+                'vicav_profiles_001', 'explain');
+    }
+    
+    /**
+     * @test
+     */
+    public function it_should_transform_a_vicav_bibliography_sousse() {
+        $this->doAssertTransformEqualsExpectedIndented("vicav_bibliography",
+                'vicav_bibl_002', 'searchRetrieve', 'vicavTaxonomy=Sousse');
+    }
+    
+    protected function doAssertTransformEqualsExpectedIndented($filename, $context, $operation, $searchOrScanClaus = null, $baseurl = null) {
+        if (!isset($baseurl)) {
+            $baseurl = 'http://corpus3.aac.ac.at/vicav2/corpus_shell//modules/fcs-aggregator/switch.php';
+        }
+        $expected = $this->getExpectedFile("$filename.html");
+        $actual = $this->getActualTransform("$filename.xml",
+                $context, $operation, $searchOrScanClaus, $baseurl);
+        $expected->xmlIndent();
+        $actual->xmlIndent();
+        $this->assertEquals($expected->saveXML(), $actual->saveXML());    }
     /**
      * @var FCSSwitchParts
      */
@@ -24,11 +72,11 @@ class XSLTTests extends XPathTestCase {
      * @var string
      */
     protected $pr;
-    
     /**
-     * @var ACDH\FCSSRU\IndentDomDocument
+     *
+     * @var array 
      */
-    protected $df;
+    protected $xsltProcess;
 
     /**
      * Sets up the fixture, for example, opens a network connection.
@@ -43,8 +91,6 @@ class XSLTTests extends XPathTestCase {
         $sru_fcs_params = new SRUWithFCSParameters('lax');
         $sru_fcs_params->xformat = 'html';
         $sru_fcs_params->recordPacking = 'raw';
-        $sru_fcs_params->operation = 'explain';
-        $sru_fcs_params->xcontext = 'arz_eng_006';
         $this->t = new FCSSwitchParts($this->pr);
         $this->t->GetDefaultStyles();
         $this->df = new IndentDomDocument();
@@ -65,10 +111,60 @@ class XSLTTests extends XPathTestCase {
     }
     
     /**
-     * @test
+     * 
+     * @param string $filename
+     * @return ACDH\FCSSRU\IndentDomDocument
      */
-    public function it_should_run() {
+    protected function getExpectedFile($filename) {
+        $ret = new IndentDomDocument();
+        $ret->setWhiteSpaceForIndentation(' ');
+        $expectedFilename = $this->pr . "xsl/tests/output-expected/fcs/$filename";
+        $expectedfile = fopen($expectedFilename, 'r');
+        $this->assertNotFalse($expectedfile, "A working expected file $filename has to exist.");
+        $ret->loadXML(fread($expectedfile , filesize($expectedFilename)));
+        fclose($expectedfile);
+        return $ret;
+    }
+    
+    /**
+     * 
+     * @global type $sru_fcs_params
+     * @param string $inputFilename
+     * @param string $xcontext
+     * @param string $operation
+     * @param string $queryOrScanClause
+     * @param string $baseUrl
+     * @return ACDH\FCSSRU\IndentDomDocument
+     */
+    protected function getActualTransform($inputFilename, $xcontext, $operation, $queryOrScanClause = null, $baseUrl = null) {
+        global $sru_fcs_params;
+        global $switchUrl;
         
-    }    
+        $sru_fcs_params->operation = $operation;
+        $sru_fcs_params->xcontext = $xcontext;
+        if (isset($queryOrScanClause)) {
+            if ($operation === 'scan') {
+                $sru_fcs_params->scanClause = $queryOrScanClause;
+            }
+            else {
+                $sru_fcs_params->query = $queryOrScanClause;
+            }
+        }
+        $savedSwitchUrl = $switchUrl;
+        if (isset($baseUrl)) {
+            $switchUrl = $baseUrl;
+        }
+        $ret = new IndentDomDocument();
+        $ret->setWhiteSpaceForIndentation(' ');
+        $configItem = $this->t->GetConfig($sru_fcs_params->xcontext);
+        $this->assertTrue(is_array($configItem), "Couldn't load config for context $xcontext!");
+        $xmlDoc = $this->t->GetDomDocument($this->pr . "xsl/tests/input/fcs/$inputFilename");
+        $this->assertNotFalse($xmlDoc, "$inputFilename not found!");
+        $xslDoc = $this->t->GetXslStyleDomDocument($sru_fcs_params->operation, $configItem);
+        $this->assertNotFalse($xslDoc, "XSL for " . $sru_fcs_params->operation . " not found!");
+        $ret->loadXML($this->t->ReturnXslT($xmlDoc, $xslDoc, true, false));
+        $switchUrl = $savedSwitchUrl;
+        return $ret;
+    }
 }
 
